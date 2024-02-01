c1a55c3fcd6456cb24c3605aa2d0fab5f0a3ab50
simonsteiger <simon.j.steiger@gmail.com>
2024-02-01 21:18:54 +0100

    Fix obs_return

--- app/logic/backend/observers.R
+++ app/logic/backend/observers.R
@@ -1,10 +1,14 @@
 box::use(
-    shiny[observeEvent],
-    shiny.router[change_page],
+  shiny[observeEvent, observe],
+  shiny.router[change_page],
+  rlang[`%||%`],
 )
 
 #' @export
 obs_return <- function(input) {
+  observe(
+    input$return %||% warning("Trying to add `obs_return()`, but missing an input with id `return`.")
+  )
   observeEvent(input$return, {
     change_page("/")
   })
